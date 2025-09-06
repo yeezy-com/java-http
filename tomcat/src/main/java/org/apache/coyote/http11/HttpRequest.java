92724066b153b2b5c82168f97cff44ea2ba1f710
GiSeok Kang <ime.rokz@gmail.com>
2025-09-06 19:18:54 +0900

    fix: HttpRequest 잘못된 로거 생성 수정

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 public final class HttpRequest {
 
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
     private final Map<String, String> params;
     private final RequestLine requestLine;
