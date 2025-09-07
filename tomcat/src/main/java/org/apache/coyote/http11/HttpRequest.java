ccfb521cff9536deae9e0ccea29308c0b91a3b41
GiSeok Kang <ime.rokz@gmail.com>
2025-09-07 18:28:33 +0900

    feat: 요청 바디 값 파싱 구현

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest.java
@@ -15,20 +15,47 @@ public final class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
     private final RequestHeader requestHeader;
+    private final Map<String, String> body;
     private final Map<String, String> params;
 
     public HttpRequest(final InputStream inputStream) {
         try {
             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String requestLine = bufferedReader.readLine();
-            log.info("{}", requestLine);
             this.requestHeader = new RequestHeader(bufferedReader);
 
-            this.requestLine = new RequestLine(requestLine);
+            // TODO: HttpMethod에 따라 분기 처리하기
+            String length = requestHeader.getHeader("Content-Length");
+            if (length != null) {
+                int contentLength = Integer.parseInt(length);
+                char[] buffer = new char[contentLength];
+                bufferedReader.read(buffer, 0, contentLength);
+                String requestBody = new String(buffer);
+
+                this.body = getBody(requestBody);
+            } else {
+                this.body = new HashMap<>();
+            }
+
             this.params = getQueryStrings();
         } catch (IOException e) {
             throw new RuntimeException("요청을 읽는데 실패했습니다.");
         }
+    }
+
+    private Map<String, String> getBody(final String requestBody) {
+        Map<String, String> body = new HashMap<>();
+
+        for (String keyValue : requestBody.split("&")) {
+            String[] keyValues = keyValue.split("=");
+            if (keyValues.length == 1) {
+                body.put(keyValues[0], "");
+                continue;
+            }
+
+            body.put(keyValues[0], keyValues[1]);
+        }
+
+        return body;
     }
 
     private Map<String, String> getQueryStrings() {
@@ -53,12 +80,12 @@ public final class HttpRequest {
     }
 
     private Optional<String> getQueryString() {
-        int index = requestLine.getUri().indexOf("?");
+        int index = requestHeader.getUri().indexOf("?");
         if (index == -1) {
             return Optional.empty();
         }
 
-        return Optional.of(requestLine.getUri().substring(index + 1));
+        return Optional.of(requestHeader.getUri().substring(index + 1));
     }
 
     public String getParam(final String key) {
@@ -66,19 +93,19 @@ public final class HttpRequest {
     }
 
     public String path() {
-        int index = requestLine.getUri().indexOf("?");
+        int index = requestHeader.getUri().indexOf("?");
         if (index == -1) {
-            return requestLine.getUri();
+            return requestHeader.getUri();
         }
 
-        return requestLine.getUri().substring(0, index);
+        return requestHeader.getUri().substring(0, index);
     }
 
     public boolean isGetMethod() {
-        return requestLine.isGet();
+        return requestHeader.isGet();
     }
 
     public boolean isPostMethod() {
-        return requestLine.isPost();
+        return requestHeader.isPost();
     }
 }
