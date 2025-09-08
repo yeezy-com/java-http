0ee9a78e6dc618133983c0f667d018916d347dea
GiSeok Kang <ime.rokz@gmail.com>
2025-09-08 15:24:20 +0900

    refactor: HTTP 응답 코드 별 메시지 정의

--- tomcat/src/main/java/org/apache/coyote/http11/response/ResponseStatus.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ResponseStatus.java
@@ -2,18 +2,20 @@ package org.apache.coyote.http11.response;
 
 public enum ResponseStatus {
 
-    OK(200),
-    FOUND(302),
-    UNAUTHORIZED(401),
+    OK(200, "OK"),
+    FOUND(302, "FOUND"),
+    UNAUTHORIZED(401, "UNAUTHORIZED"),
     ;
 
     private final int code;
+    private final String message;
 
-    ResponseStatus(int code) {
+    ResponseStatus(int code, String message) {
         this.code = code;
+        this.message = message;
     }
 
     public String parseStatusLine() {
-        return String.format("%d OK ", code);
+        return String.format("%d %s", code, message);
     }
 }
