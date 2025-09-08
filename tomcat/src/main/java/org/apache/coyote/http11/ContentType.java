cbf5477d7c38a71822e6b5d0c42d3206b5968284
GiSeok Kang <ime.rokz@gmail.com>
2025-09-08 09:41:24 +0900

    refactor: fileType과 mimeType 분리

--- tomcat/src/main/java/org/apache/coyote/http11/ContentType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ContentType.java
@@ -1,14 +1,20 @@
 package org.apache.coyote.http11;
 
 public enum ContentType {
-    HTML("html"),
-    CSS("css"),
-    JS("javascript"),
-    PLAIN("plain");
+    HTML("html", "text/html"),
+    CSS("css", "text/css"),
+    JS("js", "application/javascript"),
+    PLAIN("", "text/plain");
 
-    private final String type;
+    private final String fileType;
+    private final String mimeType;
 
-    ContentType(String type) {
-        this.type = type;
+    ContentType(String fileType, String mimeType) {
+        this.fileType = fileType;
+        this.mimeType = mimeType;
+    }
+
+    public String getMimeType() {
+        return mimeType;
     }
 }
