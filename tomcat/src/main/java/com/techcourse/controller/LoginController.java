371cfd010ef39201f2cac751bced5601de05e7b1
GiSeok Kang <ime.rokz@gmail.com>
2025-09-11 21:15:55 +0900

    fix: 로그인 페이지 경로 수정

--- tomcat/src/main/java/com/techcourse/controller/LoginController.java
+++ tomcat/src/main/java/com/techcourse/controller/LoginController.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
 
 public class LoginController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
-    private static final String LOGIN_PAGE = "login.html";
+    private static final String LOGIN_PAGE = "/login.html";
 
     @Override
     protected void doPost(HttpRequest request, HttpResponse response) throws Exception {
